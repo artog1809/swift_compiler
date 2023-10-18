34a2a8ec56efeb769aaae7dea2cdfaa26785e775
artog1809 <gdoq123@yandex.ru>
2023-10-18 23:05:34 +0300

    [fix]: fixed problems with strings rules

--- lexer/test.swift
+++ lexer/test.swift
@@ -8,23 +8,23 @@
         }
 		
 		// let myAge = 32
-		// let isOld = true
+		let isOld = true
 		let number: Int32 = 123
-		// let binNumber = 0b1010
-		// let octNumber = 0o12
+		let binNumber = 0b1010
+		let octNumber = 0o12
 		let hexNumber = 0xa1
-		// let dec = 1_1__1
+		let dec = 1_1__1
 		let oct = 0o1_2
-		// let hex = 0xa_1
+		let hex = 0xa_1
 		
-		/*
+		
 		let decFNumber = 0.123e2
 		let decMFNumber = 0.123e-2
 		let hexFNumber = 0x1.4p3
 		let decExp = 0.1_2_3e0_2
 		let hexExp1 = 0x01.04p03
 		let hexExp2 = 0x0_1.0_4p0_3
-		*/
+		
 
 		let x = 1
 		let string = "First day a month: \\(((x)))"
